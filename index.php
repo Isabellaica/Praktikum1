64b8e5a08ae8abb9906569d9a3db0dbeefe3fc20
Isabellaica <146326898+Isabellaica@users.noreply.github.com>
2024-02-24 14:24:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+$nama = 'isabella';
+$umur = 19;
+$hobi = 'baca novel';
+
+// echo 'WELCOME '. $nama .' dengan umur ' . $umur ;
+
+
+define('PHI',3.14);
+$jari = 10;
+
+$keliling = 2* PHI * $jari;
+$luas = PHI * $jari * $jari;
+
+// echo $keliling;
+// echo '<br>' . $luas;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+</body>
+</html>
